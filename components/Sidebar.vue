efa7984efba317364f0ae8a359e4d0777a3cd225
metkm <metinkorkmaz417@gmail.com>
2024-01-05 23:50:24 +0300

    wrapping

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -36,7 +36,10 @@ const ui = /*ui*/ {
       size="lg"
       class="w-full"
     >
-      <template v-if="item.children" #trailing>
+      <template 
+        v-if="item.children"
+        #trailing
+      >
         <UIcon 
           name="i-mdi-chevron-down"
           class="ml-auto transition-transform"
@@ -48,17 +51,33 @@ const ui = /*ui*/ {
 
   <DefineItems v-slot="{ items }">
     <ul>
-      <li v-for="item in items" :key="item.label">
-        <UAccordion v-if="item.children" :items="[{}]" :ui="ui">
+      <li 
+        v-for="item in items" 
+        :key="item.label"
+      >
+        <UAccordion 
+          v-if="item.children" 
+          :items="[{}]" 
+          :ui="ui"
+        >
           <template #default="{ open }">
-            <ReuseItem :item="item" :open="open" />
+            <ReuseItem 
+              :item="item" 
+              :open="open"
+            />
           </template>
 
           <template #item>
-            <ReuseItems v-if="item.children" :items="item.children" />
+            <ReuseItems 
+              v-if="item.children" 
+              :items="item.children"
+            />
           </template>
         </UAccordion>
-        <ReuseItem v-else :item="item" />
+        <ReuseItem 
+          v-else 
+          :item="item"
+        />
       </li>
     </ul>
   </DefineItems>
