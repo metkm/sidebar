66aab800869791565016a3878db936591091cc62
metkm <metinkorkmaz417@gmail.com>
2024-01-05 23:41:50 +0300

    basic sidebar

---
+++ app.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const items = [
+  {
+    label: 'To Settings',
+    icon: 'i-mdi-account-circle-outline',
+    children: [
+      {
+        label: 'User',
+        icon: 'i-mdi-brain'
+      }
+    ]
+  },
+  {
+    label: 'Some Actions',
+    icon: 'i-mdi-turkey',
+    click: () => {
+      console.log('some action')
+    }
+  }
+]
+
+const isClosed = ref(false);
+</script>
+
+<template>
+  <div class="flex h-screen">
+    <Sidebar v-model="isClosed" :items="items" />
+    
+    <main class="grow p-4">
+      <p>main content</p>
+
+      <UButton @click="isClosed = !isClosed">Toggle</UButton>
+    </main>
+  </div>
+</template>
---
+++ components/Sidebar.vue
@@ -0,0 +1,69 @@
+<script setup lang="tsx">
+import { UButton } from "#components";
+
+interface Route {
+  label: string;
+  icon: string;
+  to?: string;
+  click?: () => void;
+  children?: Route[];
+}
+
+defineProps<{
+  items: Route[];
+}>();
+
+const isClosed = defineModel<boolean>();
+
+const [DefineItems, ReuseItems] = createReusableTemplate<{ items: Route[] }>();
+const [DefineItem, ReuseItem] = createReusableTemplate<{ item: Route, open?: boolean }>();
+
+const ui = /*ui*/ {
+  item: {
+    "padding": "pb-1.5 pl-4"
+  }
+}
+</script>
+
+<template>
+  <DefineItem v-slot="{ item, open }">
+    <UButton
+      :leadingIcon="item.icon"
+      :label="!isClosed ? item.label : ''"
+      :to="item.to"
+      square
+      variant="ghost"
+      size="lg"
+      class="w-full"
+    >
+      <template v-if="item.children" #trailing>
+        <UIcon 
+          name="i-mdi-chevron-down"
+          class="ml-auto transition-transform"
+          :class="{ 'rotate-90': open }"
+        />
+      </template>
+    </UButton>
+  </DefineItem>
+
+  <DefineItems v-slot="{ items }">
+    <ul>
+      <li v-for="item in items" :key="item.label">
+        <UAccordion v-if="item.children" :items="[{}]" :ui="ui">
+          <template #default="{ open }">
+            <ReuseItem :item="item" :open="open" />
+          </template>
+
+          <template #item>
+            <ReuseItems v-if="item.children" :items="item.children" />
+          </template>
+        </UAccordion>
+        <ReuseItem v-else :item="item" />
+      </li>
+    </ul>
+  </DefineItems>
+
+  <nav class="flex flex-col justify-center h-full bg-neutral-950 p-4">
+    <ReuseItems :items="items" />
+  </nav>
+</template>
