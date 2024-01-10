b3768cf5335a6f7396470be3cb296c485de8dc9a
metkm <metinkorkmaz417@gmail.com>
2024-01-10 22:05:30 +0300

    add schema

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+import { object, lazy, string, type InferType, type ObjectSchema, Schema, StringSchema, type ISchema } from "yup";
 import { UserServices } from "~/services/UserServices";
 
-const { data: user, execute: getUser, status, isLoading } = await UserServices.getUser({
-  immediate: false
-});
+// const { data: user, execute: getUser, status, isLoading } = await UserServices.getUser({
+//   immediate: false
+// });
 
 const { data: newUser, execute: updateUser, isLoading: updateUserLoading } = await UserServices.updateUser({
   immediate: false,
@@ -12,9 +13,24 @@ const { data: newUser, execute: updateUser, isLoading: updateUserLoading } = awa
   }
 })
 
-// const { data: user, execute: getUser, status } = await useAsyncData(() => UserServices.getUser(), {
-//   immediate: false
-// })
+const getDynamicSchema = <T extends Schema>(keyValue: T) => {
+  const lazySchema = lazy((value) => {
+    let newResult = Object.keys(value).reduce((acc, key) => {
+      acc[key] = keyValue;
+      return acc
+    }, {} as Record<string, T>);
+
+    return object(newResult)
+  })
+
+  return lazySchema
+}
+
+const result = getDynamicSchema(string().required());
+type Result = InferType<typeof result>;
+  
+const validated = result.validateSync({ title: 'Title' });
+console.log(validated)
 </script>
 
 <template>
